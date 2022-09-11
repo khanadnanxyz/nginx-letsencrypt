384ce1d23464321bab4a2e7ee4d2e123a87bd8cd
Adnan Ahmed Khan <iamkhanadnan@gmail.com>
2022-09-11 16:47:24 +0600

    Updates ssl config

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(nabanna.xyz www.nabanna.xyz)
+domains=(macaw.nabanna.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="iamkhanadnan@gmail.com" # Adding a valid address is strongly recommended
